50c4ffcdabe3178748a54b786c79ac56e724e1c7
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-27 18:06:42 +0900

    __dirname, __filename

--- 02_node_basic/030_write_file/start/example.js
+++ 02_node_basic/030_write_file/start/example.js
@@ -1,1 +1,11 @@
-console.log('hello, node.js');
+// ファイルへの書き込み
+// __dirname, __filename
+
+// common jsのインポートの方法
+const fs = require('fs');
+
+// console.log(__dirname);
+// console.log(__filename);
+
+fs.writeFileSync(__dirname + '/test.txt', 'hello, node.js');
+// console.log('hello, node.js');
