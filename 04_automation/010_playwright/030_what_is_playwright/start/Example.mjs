f7141597fc5210aca7f4f8ddbdd327acaf0cc11b
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-08 23:14:49 +0900

    スクレイピングの記述

--- 04_automation/010_playwright/030_what_is_playwright/start/Example.mjs
+++ 04_automation/010_playwright/030_what_is_playwright/start/Example.mjs
@@ -1,2 +1,12 @@
 import { chromium } from "@playwright/test";
 
+const browser = await chromium.launch();
+const page = await browser.newPage();
+await page.goto("https://localhost:3000");
+const htmlStr = await page.content();
+
+console.log(htmlStr);
+
+
+
+
