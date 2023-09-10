67ebbbac97e23347b421764a222501c0e80e63bd
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-10 14:50:42 +0900

    res.writeHead

--- 06_backend/010_http_server/start/app.mjs
+++ 06_backend/010_http_server/start/app.mjs
@@ -6,27 +6,21 @@ import * as http from "http";
 
 const server = http.createServer(function (req, res){
     console.log(req.url);
-    // res.writeHead(200,{'content-type': 'text/html; charset=UTF-8'});
+    // res.writeHead(302,{location: '/redirected'});
     //ブラウザ上でしか動作しないので、記述するとクラッシュする
     // window.alert("メッセージ");
 
     if (req.url === '/hello') {
-        // res.end('<h1>こんにちは</h1>');
-        //res.endは1回だけしか呼べない
-        res.end(`<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-<h1>こんにちは</h1>
-</body>
-</html>`)
+        //helloでアクセスしたら 302レスポンスの/redirectedへリダイレクトするようにする
+        res.writeHead(302, {location: '/redirected'});
+        //そのため、ここには行かない
+        res.end('<h1>こんにちは</h1>');
     } else if (req.url === '/bye') {
         res.end('bye');
-    }
-});
+    } else {
+        //redirectedされたURLを返す
+        res.end(req.url);
+    }});
 
 // サーバーが立ち上がる
 server.listen(8080);
