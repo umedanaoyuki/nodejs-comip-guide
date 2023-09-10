6309e9f5198564d7e901deb277b8f87e526fde61
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-10 14:27:41 +0900

    html內に文字コードを記述する

--- 06_backend/010_http_server/start/app.mjs
+++ 06_backend/010_http_server/start/app.mjs
@@ -6,12 +6,23 @@ import * as http from "http";
 
 const server = http.createServer(function (req, res){
     console.log(req.url);
-    res.writeHead(200,{'content-type': 'text/html; charset=UTF-8'});
+    // res.writeHead(200,{'content-type': 'text/html; charset=UTF-8'});
     //ブラウザ上でしか動作しないので、記述するとクラッシュする
     // window.alert("メッセージ");
 
     if (req.url === '/hello') {
-        res.end('<h1>こんにちは</h1>');
+        // res.end('<h1>こんにちは</h1>');
+        //res.endは1回だけしか呼べない
+        res.end(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>こんにちは</h1>
+</body>
+</html>`)
     } else if (req.url === '/bye') {
         res.end('bye');
     }
