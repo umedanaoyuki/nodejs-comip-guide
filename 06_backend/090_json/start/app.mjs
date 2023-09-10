9eac976dd83df7332a3d460571b58022b95afe80
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-10 17:08:49 +0900

    JSONを返却する実装

--- 06_backend/090_json/start/app.mjs
+++ 06_backend/090_json/start/app.mjs
@@ -4,8 +4,10 @@ import express from 'express';
 const PORT = 8080;
 const app = express();
 
+// JSON.stringify({message: 'hello', number: 1, array: ['banana','orange', 1]})
 app.get('/', function(req, res) {
-    res.send('hello');
+    //必ずJsonで渡したい場合の書き方
+    res.json({message: 'hello', number: 1, array: ['banana','orange', 1]});
 })
 
 app.listen(PORT, function() {
