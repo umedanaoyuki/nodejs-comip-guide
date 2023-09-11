f3128c25e4994ea287951d346b4d05f79366ab37
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-11 22:53:21 +0900

    JSONを受け取る実装

--- 06_backend/100_recieve_json/start/app.mjs
+++ 06_backend/100_recieve_json/start/app.mjs
@@ -1,8 +1,10 @@
 import * as http from 'http';
 import express from 'express';
 
-const PORT = 8080;
+const PORT = 3000;
 const app = express();
+
+app.use(express.json());
 
 app.get('/', function (req, res) {
   res.send(`
@@ -11,12 +13,39 @@ app.get('/', function (req, res) {
       <input type="text" name="description">
       <input type="submit">
     </form>
+    <script>
+        const formEl = document.querySelector('form');
+        formEl.onSubmit = (e) =>  {
+            //ページ遷移をするアクションを止める処理
+            e.preventDefault();
+            
+            const title = formEl[0].value;
+            const desc = formEl[1].value;
+            
+            const data = {
+                title,
+                desc,
+            };
+            
+            fetch('/result', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(data) 
+            }).then(async function (res)) {
+                const data = await res.json();
+                console.log(data);
+            });
+        }
+    </script>
     `);
 });
 
 app.post('/result', function (req, res) {
   const params = req.body;
   console.log(params);
+  res.json({msg: 'success'});
 });
 
 app.listen(PORT, function () {
